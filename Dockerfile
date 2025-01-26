c301b9bd5f0a8c4a0a36bfaf95a5f95f5e5eecf3
Rhenar <hugo.chassaing@orange.fr>
2025-01-26 17:40:12 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,21 @@ FROM brunoric/hhvm:deb-hhvm
 
 ENV CTF_PATH /var/www/fbctf
 ENV DEBIAN_FRONTEND noninteractive
-ENV CTF_REPO https://github.com/facebook/fbctf.git
+ENV CTF_REPO https://github.com/rhenar0/fbctf
 
 RUN apt-get update && apt-get install -y --force-yes curl language-pack-en git npm nodejs-legacy nginx mysql-client
 
+#RUN wget http://ports.ubuntu.com/pool/main/c/ca-certificates/ca-certificates_20240203~20.04.1_all.deb
+RUN apt install ca-certificates
+
+RUN update-ca-certificates
+
 # Install Composer
-RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+#RUN curl --insecure -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+RUN wget https://getcomposer.org/download/latest-1.x/composer.phar
+RUN chmod a+x composer.phar
+RUN mv composer.phar /usr/bin/composer.phar
 
 RUN mkdir -p $CTF_PATH
 WORKDIR $CTF_PATH
@@ -16,10 +25,16 @@ WORKDIR $CTF_PATH
 RUN git clone --depth 1 $CTF_REPO .
 
 # Install Vendors
-RUN composer install
+RUN composer.phar install
+
+RUN npm cache clean -f
+RUN npm install -g n
+RUN node --version
+
+RUN n 10.12.0
 
 # Build assets
-RUN npm install && npm install -g grunt && npm install -g flow-bin
+RUN npm install && npm install -g grunt@1.0.1 && npm install -g flow-bin@0.35.0
 RUN grunt
 
 # Add nginx configuration
--- entrypoint.sh
+++ entrypoint.sh
@@ -14,31 +14,35 @@ cat "$CTF_PATH/extra/hhvm.conf" | sed "s|CTFPATH|$CTF_PATH/|g" | tee /etc/hhvm/s
 chown -R www-data:www-data /var/www/*
 rm /etc/nginx/sites-enabled/*
 
-if ${SSL_SELF_SIGNED:=true}; then
-  echo "Generating self-signed certificate..."
-  __country=${SSL_COUNTRY:-"UK"}
-  __city=${SSL_CITY:-"London"}
-  __url=${CTF_URL:-"example.com"}
-  __email=${SSL_EMAIL:-"dev@$__url"}
+#if ${SSL_SELF_SIGNED:=true}; then
+#  echo "Generating self-signed certificate..."
+#  __country=${SSL_COUNTRY:-"UK"}
+#  __city=${SSL_CITY:-"London"}
+#  __url=${CTF_URL:-"example.com"}
+#  __email=${SSL_EMAIL:-"dev@$__url"}
 
   # Generating self signed cert
-  mkdir -p /etc/nginx/certs/
-  cd /etc/nginx/certs/
-  openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
-  openssl rsa -passin pass:x -in server.pass.key -out server.key
-  rm server.pass.key
-  openssl req -new -key server.key -out server.csr \
-    -subj "/C=$__country/ST=NRW/L=$__city/O=My Inc/OU=DevOps/CN=www.$__url/emailAddress=$__email"
-  openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
-  openssl dhparam -out dhparam.pem 2048
-  cd - > /dev/null # restore directory
+#  mkdir -p /etc/nginx/certs/
+#  cd /etc/nginx/certs/
+#  openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
+#  openssl rsa -passin pass:x -in server.pass.key -out server.key
+#  rm server.pass.key
+#  openssl req -new -key server.key -out server.csr \
+#    -subj "/C=$__country/ST=NRW/L=$__city/O=My Inc/OU=DevOps/CN=www.$__url/emailAddress=$__email"
+#  openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+#  openssl dhparam -out dhparam.pem 2048
+#  cd - > /dev/null # restore directory
 
-  cat "/etc/nginx/sites-available/fbctf_ssl.tmpl.conf" | sed "s|CTFPATH|$CTF_PATH/src|g" | tee /etc/nginx/sites-available/fbctf-ssl.conf > /dev/null
-  ln -s /etc/nginx/sites-available/fbctf-ssl.conf /etc/nginx/sites-enabled/fbctf-ssl.conf
-else
-  ln -s /etc/nginx/sites-available/fbctf.conf /etc/nginx/sites-enabled/fbctf.conf
-  sed -i -r -e '/private static bool \$s_secure/ {s/true/false/}' $CTF_PATH/src/SessionUtils.php
-fi
+#  cat "/etc/nginx/sites-available/fbctf_ssl.tmpl.conf" | sed "s|CTFPATH|$CTF_PATH/src|g" | tee /etc/nginx/sites-available/fbctf-ssl.conf > /dev/null
+#  ln -s /etc/nginx/sites-available/fbctf-ssl.conf /etc/nginx/sites-enabled/fbctf-ssl.conf
+#else
+#  ln -s /etc/nginx/sites-available/fbctf.conf /etc/nginx/sites-enabled/fbctf.conf
+#  sed -i -r -e '/private static bool \$s_secure/ {s/true/false/}' $CTF_PATH/src/SessionUtils.php
+#fi
+
+## Config NGINX
+ln -s /etc/nginx/sites-available/fbctf.conf /etc/nginx/sites-enabled/fbctf.conf
+sed -i -r -e '/private static bool \$s_secure/ {s/true/false/}' $CTF_PATH/src/SessionUtils.php
 
 # Forward request and error logs to docker log collector
 ln -sf /dev/stdout /var/log/nginx/access.log \
